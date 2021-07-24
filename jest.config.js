fec3280adcf93e31b9496ecb7f6ae43a96791bbc
syczxs <2274336077@qq.com>
2021-07-24 17:49:01 +0800

    change circleci config

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,18 @@
 module.exports = {
     verbose: true,
     clearMocks: false,
-    collectCoverage: true,
     reporters: ["default","jest-junit"],
+
+    collectCoverage: false,
+
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     moduleDirectories: ['node_modules'],
+
+    globals: {
+      'ts-jest': {
+        tsConfig: 'tsconfig.test.json',
+      },
+    },
     moduleNameMapper: {
       "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
     "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
